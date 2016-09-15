43ce403d811caeb50bc94005d9e792df616035fa
abelfacenda <abelfacenda26@gmail.com>
2016-09-15 12:21:25 -0400

    Linked the book's covers with the cache folder

--- src/app/BookCover.php
+++ src/app/BookCover.php
@@ -10,20 +10,37 @@
 class BookCover
 {
     private $syndeticsClientCode;
+    private $googleBooksKey;
     private $isbn;
     private $imageSize;
 
-    public function __construct($syndeticsClientCode, $isbn, $imageSize) {
+    public function __construct($syndeticsClientCode, $isbn, $imageSize, $googleBooksKey) {
         $this->syndeticsClientCode = $syndeticsClientCode;
+        $this->googleBooksKey = $googleBooksKey;
         $this->isbn = $this->validateISBN($isbn);
         $this->imageSize = $imageSize;
     }
 
     function getCover(){
-        $result = $this->getCoverFromSyndetics();
+		
+        $result = '';
+        $cover_path = $_SERVER["DOCUMENT_ROOT"]."/external_scripts/newitems/cover_cache/".$this->isbn.".jpg";
+		
+        if ($this->isCoverInChache($cover_path)){
+            $result = "http://sp.library.miami.edu/external_scripts/newitems/cover_cache/".$this->isbn.".jpg";
+        }else if (empty($result)) {
+            $foundInSyndetics = $this->getCoverFromSyndetics($cover_path);
 
-        if (empty($result))
-            $result = $this->getCoverFromGoogleBooks();
+            if ($foundInSyndetics) {
+                $result = "http://sp.library.miami.edu/external_scripts/newitems/cover_cache/".$this->isbn.".jpg";
+            } else {
+                $foundInGoogle = $this->getCoverFromGoogleBooks($cover_path);
+
+                if ($foundInGoogle) {
+                    $result = "http://sp.library.miami.edu/external_scripts/newitems/cover_cache/".$this->isbn.".jpg";
+                }
+            }
+        }
 
         return $result;
     }
@@ -41,36 +58,27 @@ class BookCover
         return $result;
     }
 
-    private function getCoverFromSyndetics()
+    private function getCoverFromSyndetics($local_destination)
     {
-        $result = '';
-
         if (!empty($this->isbn) && !empty($this->imageSize) && !empty($this->syndeticsClientCode)) {
 
             $imageUrl = 'https://syndetics.com/index.aspx?isbn=' . $this->isbn . '/' . $this->imageSize . '.JPG&client=' . $this->syndeticsClientCode;
 
-            $imageInfo = @getimagesize($imageUrl);
+            $imageValidated = $this->validateImageExists($this->isbn, $this->syndeticsClientCode);
 
-
-            if ($imageInfo) {
-                if (isset($imageInfo['mime'])) {
-                    if ($imageInfo['mime'] === 'image/jpeg') {
-                        $result = $imageUrl;
-                    }
-                }
+            if ($imageValidated != false) {
+                $this->download_cover($imageUrl, $local_destination);
+                return true;
             }
         }
-
-        return $result;
+        return false;
     }
 
-    private function getCoverFromGoogleBooks()
+    private function getCoverFromGoogleBooks($local_destination)
     {
-        $result = '';
-
-        if (!empty($this->isbn)) {
+        if (!empty($this->isbn) && !empty($this->googleBooksKey)) {
             $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, 'https://www.googleapis.com/books/v1/volumes?q=isbn:' . $this->isbn);
+            curl_setopt($curl, CURLOPT_URL, 'https://www.googleapis.com/books/v1/volumes?key='.$this->googleBooksKey.'&q=isbn:' . $this->isbn);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             $data = json_decode(curl_exec($curl));
@@ -80,15 +88,65 @@ class BookCover
                 if (isset($data->totalItems)) {
                     if ($data->totalItems > 0) {
                         if (isset($data->items[0]->volumeInfo->imageLinks->thumbnail)) {
-                            $result = $data->items[0]->volumeInfo->imageLinks->thumbnail;
+                            $this->download_cover($data->items[0]->volumeInfo->imageLinks->thumbnail, $local_destination);
+                            return true;
                         }
                     }
                 }
             }
         }
 
-        return $result;
+        return false;
+    }
 
+    private function isCoverInChache($cover_local_path) {
+
+        if(file_exists('file://' . $cover_local_path)) {
+            return true;
+        }
+		
+        return false;
+    }
+
+    private function download_cover($url, $local_destination) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $raw=curl_exec($curl);
+        curl_close($curl);
+        file_put_contents($local_destination, $raw);
+    }
+
+    private function validateImageExists($isbn, $syndeticsClientCode)
+    {
+        $xmlUrl = 'https://syndetics.com/index.aspx?isbn=' . $isbn . '/xml.xml&client=' . $syndeticsClientCode . '&type=rn12';
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $xmlUrl);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($curl);
+        $xml = null;
+        libxml_use_internal_errors(true);
+        $xml = simplexml_load_string($result);
+
+        curl_close($curl);
+
+        if (isset($xml)) {
+            if (isset($xml->LC)) {
+                return $xml->LC;
+            } elseif (isset($xml->MC)) {
+                return $xml->MC;
+            } elseif (isset($xml->SC)) {
+                return $xml->SC;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        libxml_use_internal_errors(false);
     }
 
 }
